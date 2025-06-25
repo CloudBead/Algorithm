f53effc64102825aa78cf1cb3972d159abdf0b12
koo1029 <145544644+koo1029@users.noreply.github.com>
2025-06-25 17:14:56 +0900

    [Bronze II] Title: 수 정렬하기, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int n;
+    int num[1000];
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++){
+        cin >> num[i];
+    }
+
+    sort(num, num + n);
+
+    for (int i = 0; i < n; i++){
+        cout << num[i] << endl;
+    }
+}
