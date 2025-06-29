73e8a05d84224edac3f5c413cc688a61ce5bfe3b
koo1029 <145544644+koo1029@users.noreply.github.com>
2025-06-29 17:20:12 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 72 ms, Memory: 9948 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <set>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    set<string> s;
+    string name, state;
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++){
+        cin >> name >> state;
+        if (state == "enter") s.insert(name);
+        else s.erase(name);
+    }
+
+    for (auto j = s.rbegin(); j != s.rend(); ++j){
+        cout << *j << '\n';
+    }
+}
