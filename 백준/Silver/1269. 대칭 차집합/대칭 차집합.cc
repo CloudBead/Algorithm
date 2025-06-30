8a4e0e4eb699acff9761172fe57227218e225a38
koo1029 <145544644+koo1029@users.noreply.github.com>
2025-06-30 14:39:13 +0900

    [Silver IV] Title: 대칭 차집합, Time: 200 ms, Memory: 20768 KB -BaekjoonHub

---
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, m, cnt = 0;
+    int tmp;
+    map<int, int> list;
+
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        cin >> tmp;
+        list[tmp]++;
+        cnt++;
+    }
+
+    for (int j = 0; j < m; j++){
+        cin >> tmp;
+        list[tmp]++;
+        cnt++;
+        if (list[tmp] == 2) cnt -= 2;
+    }
+
+    cout << cnt << '\n';
+}
