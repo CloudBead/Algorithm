22c453c14132432b98158967192fe735c2793bc9
CloudBead <145544644+CloudBead@users.noreply.github.com>
2025-07-02 02:30:22 +0900

    [Silver II] Title: 베르트랑 공준, Time: 12 ms, Memory: 2172 KB -BaekjoonHub

---
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    vector<bool> isPrime(246913, true);
+    isPrime[0] = isPrime[1] = false;
+
+    // 에라토스테네스의 체
+    for (int i = 2; i * i < 246913; ++i) {
+        if (isPrime[i]) {
+            for (int j = i * i; j < 246913; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+
+    int n;
+    while (true) {
+        cin >> n;
+        if (n == 0) break;
+
+        int cnt = 0;
+        for (int i = n + 1; i <= 2 * n; ++i) {
+            if (isPrime[i]) cnt++;
+        }
+
+        cout << cnt << '\n';
+    }
+}
