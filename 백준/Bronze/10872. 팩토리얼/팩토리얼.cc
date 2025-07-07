b5bd6fc2f0b0045867a2a45cdf3eaf1facb3b9c4
CloudBead <145544644+CloudBead@users.noreply.github.com>
2025-07-07 11:40:03 +0900

    [Bronze III] Title: 팩토리얼, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int f(int a) {
+	if (a == 0) return 1;
+	return a * f(a - 1);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+	int n;
+	cin >> n;
+	cout << f(n);
+}
