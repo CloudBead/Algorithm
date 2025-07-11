e13341c8a775468394eb5cad6a7b32156897c66f
CloudBead <145544644+CloudBead@users.noreply.github.com>
2025-07-12 00:54:37 +0900

    [Silver III] Title: 칸토어 집합, Time: 24 ms, Memory: 3604 KB -BaekjoonHub

---
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void cantor(char* A, int start, int len){
+    if (len == 1) return;
+
+    int third = len / 3;
+
+    for (int i = start + third; i < start + 2 * third; ++i){
+        A[i] = ' ';
+    }
+
+    cantor(A, start, third);
+    cantor(A, start + 2 * third, third);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+	int N;
+
+    while (cin >> N){
+        int len = pow(3,N);
+        char* res = new char[len + 1];
+
+        for (int i = 0; i < len; i++) {
+                res[i] = '-';
+        }
+
+        cantor(res, 0, len);
+
+        for (int i = 0; i < len; i++){
+            cout << res[i];
+        }
+        cout << "\n";
+    }
+}
