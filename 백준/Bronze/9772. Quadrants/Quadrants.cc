1098403d7079c6db2a340713645fbf4c09bff5a2
CloudBead <145544644+CloudBead@users.noreply.github.com>
2025-09-02 20:38:47 +0900

    [Bronze IV] Title: Quadrants, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9772. Quadrants/Quadrants.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    double x, y;
+    
+    while (true){
+        
+        cin >> x >> y;
+        
+        if (x == 0 && y == 0) break;
+        
+        if (x == 0 || y == 0) cout << "AXIS\n";
+        else if (x > 0) {
+            if (y > 0) cout << "Q1\n";
+            else cout << "Q4\n";
+        }
+        else {
+            if (y > 0) cout << "Q2\n";
+            else cout << "Q3\n";
+        }
+    }
+    
+    cout << "AXIS";
+
+}
