302f247c263bd00094c501e8e80c2dee779ec01e
CloudBead <145544644+CloudBead@users.noreply.github.com>
2025-09-25 23:45:31 +0900

    [Bronze V] Title: 삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    double W, H;
+    cin >> W >> H;
+
+    double area = W * H / 2.0;
+    cout << fixed << setprecision(1) << area << '\n';
+}
