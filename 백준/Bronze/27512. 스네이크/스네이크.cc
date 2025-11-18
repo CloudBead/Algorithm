64abca26a7e3e738034db2c77f4e09218f2848dd
CloudBead <145544644+CloudBead@users.noreply.github.com>
2025-11-19 00:14:30 +0900

    [Bronze II] Title: 스네이크, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27512. 스네이크/스네이크.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  if (n % 2 == 0 || m % 2 == 0)
+    cout << n * m;
+  else
+    cout << n * m - 1;
+  return 0;
+}
