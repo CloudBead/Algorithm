812a58ebd268cfdeee8fd1e1b8b2ee58b4bc6cbd
CloudBead <145544644+CloudBead@users.noreply.github.com>
2025-11-20 17:04:50 +0900

    [Bronze V] Title: Football Scoring, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24736. Football Scoring/Football Scoring.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b, c, d, e;
+    
+    for (int i = 0; i < 2; ++i){
+        cin >> a >> b >> c >> d >> e ;
+        cout << a * 6 + b * 3 + c * 2 + d + e * 2 << " ";
+    }
+}
