ff29595ec1a3131d9186c5462b87ada92be62503
CloudBead <145544644+CloudBead@users.noreply.github.com>
2025-11-24 23:53:00 +0900

    [Bronze IV] Title: Haughty Cuisine, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/20336. Haughty Cuisine/Haughty Cuisine.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    string tmp;
+    
+    cin >> n;
+    
+    cin >> n;
+    
+    cout << n << "\n";
+    
+    for (int i = 0; i < n; i++){
+        cin >> tmp;
+        
+        cout << tmp << "\n";
+    }
+}
