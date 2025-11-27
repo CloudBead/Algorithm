33c0321ca87b04b074f8000a69824750156d5486
CloudBead <145544644+CloudBead@users.noreply.github.com>
2025-11-27 21:16:27 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string tmp;
+    cin >> tmp;
+    cout << ":fan::fan::fan:\n";
+    cout << ":fan::" << tmp << "::fan:\n";
+    cout << ":fan::fan::fan:";
+}
