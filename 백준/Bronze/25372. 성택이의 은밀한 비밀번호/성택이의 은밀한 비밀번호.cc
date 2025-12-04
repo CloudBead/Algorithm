b9f60cf40143d8c300f536c721e58f1c990dc218
CloudBead <145544644+CloudBead@users.noreply.github.com>
2025-12-04 21:11:43 +0900

    [Bronze V] Title: 성택이의 은밀한 비밀번호, Time: 28 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/25372. 성택이의 은밀한 비밀번호/성택이의 은밀한 비밀번호.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    string s;
+    
+    cin >> n;
+    
+    for (int i = 0; i < n; ++i){
+        cin >> s;
+        if (s.length() < 6 || s.length() > 9) cout << "no\n";
+        else cout << "yes\n";
+    }
+}
